7f156c2faf906055b2853191ceb395180c743da6
Atops93 <aetops93@proton.me>
2024-08-22 18:29:10 +0930

    add picom conf

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo pacman -S libx11 libxft libxinerama
+cd dwm
+sudo make clean install
+cd ..
+echo "Do you want to replace the current .xinitrc if located with mine? (y/n)"
+if [[y]] ; then
+	sudo cp .xinitrc /home/$USER
+continue
+
+echo "Moving my picom.conf file to /etc/xdg replacing a configured picom? (y/n)"
+if [[y]] ; then
+	sudo cp picom.conf /etc/xdg/
+echo "Your all good to go, run startx & it should run"
