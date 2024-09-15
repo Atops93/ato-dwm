22cd79cbfd9e0ccd3feb379bebbf550c2ea8f81e
Atops93 <aetops93@proton.me>
2024-09-15 13:32:41 +0930

    switched bar & removed old one due to centering issues

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
-sudo pacman -S xorg-server xorg-xinit libx11 libxft libxinerama xclip feh flameshot rofi brightnessctl picom
-cd dwm
-sudo make clean install
-cd ..
-echo "Do you want to replace your current .xinitrc if you created it, with mine? (y/n)"
-if [[y]] ; then
-	sudo cp .xinitrc /home/$USER
-continue
+sudo pacman -S xorg-server xorg-xinit libx11 libxft libxinerama xclip feh flameshot rofi brightnessctl picom alacritty
+cd dwm; sudo make clean install
+cd ~/ato-dwm; mv alacritty rofi ~/.config
+until [ "$replace" = "y"] || [ "$replace" = "n" ]; do
+echo -n "Do you want to replace your current .xinitrc if you created it, with mine? (y/n)"
+read -r replace
+done
 
-echo "Moving my picom.conf file to /etc/xdg replacing a configured picom? (y/n)"
-if [[y]] ; then
-	sudo pacman -S picom && sudo cp picom.conf /etc/xdg/
-echo "All good to go, run startx & it should run"
+if [ "$replace" = "y"] ; then
+	sudo cp .xinitrc /home/$USER
+fi
+
+until [ "$picominstall" = "y" ] || [ "$picominstall" = "n" ]; do
+echo -n "Moving my picom.conf file to /etc/xdg replacing a configured picom? (y/n)"
+read -r picominstall
+done
+if [ "$picominstall" = "y" ]; then
+	cp picom.conf /etc/xdg/
+else
+	sudo cp picom.conf /etc/xdg/
+fi
+echo "All good! Run startx & it should run"
